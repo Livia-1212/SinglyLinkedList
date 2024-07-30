a4b40613cddd70e89ff46f65d676e88514b054ee
Livia Li <liviali@Livias-iMac.local>
2024-07-29 21:36:51 -0400

    CircularlinkedList

---
+++ src/CircularLinkedList.java
@@ -0,0 +1,71 @@
+public class CircularLinkedList <E> {
+    private static class Node<E>{
+        private E element;
+        private Node<E> next;
+        public Node(E e, Node<E> n){
+            element = e;
+            next = n;
+        }
+        public E getElement(){return element;}
+        public Node<E> getNext(){return next;}
+        public void setNext(Node<E> n){next = n;}
+    }
+
+    private Node<E> tail = null;
+    private int size = 0;
+    public CircularLinkedList(){}
+
+    public int size(){return size;}
+    public boolean isEmpty(){return size == 0;}
+
+    public E first(){
+        if(isEmpty())return null;
+        return tail.getNext().getElement();
+    }
+
+    public E last(){
+        if(isEmpty()) return null;
+        return tail.getElement();
+    }
+
+    public void rotate(){
+        if(tail != null)
+            tail = tail.getNext();
+    }
+    public void addFirst(E e){
+        if(size == 0){
+            tail = new Node<>(e, null);
+            tail.setNext(tail);
+        }else{
+            Node<E> newest = new Node<>(e, tail.getNext());
+            tail.setNext(newest);
+        }
+        size++;
+    }
+
+    public void addLast(E e){
+        addFirst(e);
+        tail = tail.getNext();
+    }
+
+    public E removeFirst(){
+        if(isEmpty()) return null;
+        Node<E> head = tail.getNext();
+        if(head == tail)
+            tail = null;
+        else tail.setNext(head.getNext());
+        size--;
+        return head.getElement();
+    }
+
+    public String displayList() {
+        StringBuilder builder = new StringBuilder();
+        CircularLinkedList.Node current = tail;
+        while (current != null) {
+            builder.append(current.getElement()).append("-->");
+            current = current.getNext();
+        }
+        builder.append("null");
+        return builder.toString();
+    }
+}
